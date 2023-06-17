7b8d28a2756a777036fc6826513d427a915bff89
Simon Erkelens <github@casa-laguna.net>
2023-06-18 11:45:09 +1200

    Fix config namespace, add SS5 support (#6)
    Closes #1
    
    Co-authored-by: Jono Menz <jonomenz@gmail.com>

--- src/EnvironmentAwareness.php
+++ src/EnvironmentAwareness.php
@@ -2,10 +2,11 @@
 
 namespace JonoM\EnvironmentAwareness;
 
+use SilverStripe\Core\Config\Configurable;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Environment;
-use SilverStripe\Security\Permission;
 use SilverStripe\Security\Member;
+use SilverStripe\Security\Permission;
 use SilverStripe\Security\Security;
 use SilverStripe\View\TemplateGlobalProvider;
 
@@ -14,29 +15,48 @@ use SilverStripe\View\TemplateGlobalProvider;
  */
 class EnvironmentAwareness implements TemplateGlobalProvider
 {
+    use Configurable;
+
+    public const ENV_LABEL = 'SS_ENVIRONMENT_LABEL';
+
     public static function getEnvironments()
     {
-        return Config::inst()->get('EnvironmentAwareness', 'environments');
+        // Legacy fallback
+        $envs = Config::inst()->get('EnvironmentAwareness', 'environments');
+        if (!$envs) {
+            $envs = self::config()->get('environments');            
+        }
+
+        return $envs;
     }
 
     public static function EnvironmentLabel()
     {
-        return Environment::getEnv('SS_ENVIRONMENT_LABEL');
+        return Environment::getEnv(self::ENV_LABEL);
     }
 
     public static function getEnvironmentProp($prop, $fallback = null)
     {
         $label = self::EnvironmentLabel();
-        if (!$label) return false;
+
+        if (!$label) {
+            return false;
+        }
         $envs = self::getEnvironments();
-        if (!array_key_exists($label, $envs) || !array_key_exists($prop, $envs[$label])) return $fallback;
+        if (!array_key_exists($label, $envs) || !array_key_exists($prop, $envs[$label])) {
+            return $fallback;
+        }
+
         return $envs[$label][$prop];
     }
 
     public static function EnvironmentShortLabel()
     {
         $shortLabel = self::getEnvironmentProp('short_label');
-        if (!$shortLabel && $label = self::EnvironmentLabel()) $shortLabel = substr($label,0,1);
+        if (!$shortLabel && $label = self::EnvironmentLabel()) {
+            $shortLabel = substr($label, 0, 1);
+        }
+
         return $shortLabel;
     }
 
@@ -52,14 +72,19 @@ class EnvironmentAwareness implements TemplateGlobalProvider
 
     public static function ShowEnvironmentNotice()
     {
-        if (!self::EnvironmentLabel() || !Permission::check("CMS_ACCESS")) return false;
-        $members = Config::inst()->get('EnvironmentAwareness', 'Members');
+        if (!self::EnvironmentLabel() || !Permission::check("CMS_ACCESS")) {
+            return false;
+        }
+        $members = self::config()->get('Members');
         // If no specific members are set, show to all CMS users
-        if (!$members) return true;
+        if (!$members) {
+            return true;
+        }
         // Otherwise only show to indicated members
         $member = Security::getCurrentUser();
-        $identifierField = Member::config()->unique_identifier_field;
-        return ($member && is_array($members)) ? in_array($member->{$identifierField}, $members) : false;
+        $identifierField = Member::config()->get('unique_identifier_field');
+
+        return $member && is_array($members) && in_array($member->{$identifierField}, $members);
     }
 
     public static function get_template_global_variables()
